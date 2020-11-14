857e930d47f0ce061ce07d25f5a30188c933b041
NwWwK <sandouly.s@gmail.com>
2020-11-14 12:15:33 +0000

    Récupération array API

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -4,7 +4,9 @@
 namespace App\Controller;
 
 
+use http\Client\Request;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpClient\HttpClient;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -15,6 +17,25 @@ class MainController extends AbstractController
      * @Route(path="/", name="home")
      */
     function home(): Response {
-        return $this->render('base.html.twig');
+
+        $client = HttpClient::create();
+
+        $nomPokmn = "salamence";
+
+        $url = "https://pokeapi.co/api/v2/pokemon/${nomPokmn}";
+
+        $response = $client->request('GET', $url);
+
+        if (200 !== $response->getStatusCode()) {
+            throw new \RuntimeException(sprintf('The API return an error.'));
+        }
+
+        $apiResponse = $response->toArray();
+
+        var_dump($apiResponse);
+
+        return $this->render('base.html.twig', [
+            'apiResponse' => $apiResponse
+        ]);
     }
 }
