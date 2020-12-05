47a5691a04e01756c3a466485f4324e336b8612c
Kevin Tourret <tourret.kevin@gmail.com>
2020-12-05 16:36:29 +0100

    Add content into eggroup manager

--- src/Manager/Pokedex/EggGroupManager.php
+++ src/Manager/Pokedex/EggGroupManager.php
@@ -5,8 +5,35 @@ namespace App\Manager\Pokedex;
 
 
 use App\Manager\AbstractManager;
+use App\Manager\Api\ApiManager;
+use App\Manager\TextManager;
+use App\Repository\Moves\DamageClassRepository;
+use App\Repository\Pokedex\EggGroupRepository;
+use Doctrine\ORM\EntityManagerInterface;
 
 class EggGroupManager extends AbstractManager
 {
+    /**
+     * @var EggGroupRepository
+     */
+    private EggGroupRepository $eggGroupRepository;
 
+    /**
+     * PokemonManager constructor.
+     *
+     * @param EntityManagerInterface $entityManager
+     * @param EggGroupRepository $eggGroupRepository
+     * @param ApiManager $apiManager
+     * @param TextManager $textManager
+     */
+    public function __construct(
+        EntityManagerInterface $entityManager,
+        EggGroupRepository $eggGroupRepository,
+        ApiManager $apiManager,
+        TextManager $textManager
+    )
+    {
+        $this->eggGroupRepository = $eggGroupRepository;
+        parent::__construct($entityManager, $apiManager, $textManager);
+    }
 }
