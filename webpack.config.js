1d39506cbcdb38d1778a32d04145dba5c4930efd
Damien Citaire <damien.citaire@gmail.com>
2021-02-24 00:16:32 +0100

    refactor: begin to re-order scss files, split addStyleEntry in two parts (libs and style), make footer fixed to the bottom of the page

--- webpack.config.js
+++ webpack.config.js
@@ -23,7 +23,9 @@ Encore
      * Each entry will result in one JavaScript file (e.g. app.js)
      * and one CSS file (e.g. app.css) if your JavaScript imports CSS.
      */
-    .addStyleEntry('styles', './assets/styles.scss')
+    .addStyleEntry('style', './assets/styles/index.scss')
+    .addStyleEntry('libs', './assets/styles/libs.scss')
+
     .addEntry('scripts', './assets/scripts.js')
     //.addEntry('page2', './assets/page2.js')
 
