868e39b05f3ae294dfeff277eca3733038e562a5
Damien Citaire <damien.citaire@gmail.com>
2021-02-26 19:15:21 +0100

    refactor + feat: continue style refactoring, big graphic update on pkmn profile ans pokedex pages

--- assets/script/tabnav/sticky_nav.js
+++ assets/script/tabnav/sticky_nav.js
@@ -2,18 +2,18 @@
 /**
  * Handle the sticky navbar
  */
-window.addEventListener('load', () => {
+// window.addEventListener('load', () => {
 
-    window.addEventListener('scroll', (event) => {
-        let sectionNav = document.getElementById('section-nav-menu');
-        let sticky = sectionNav.offsetTop;
-        // whether that's below the form
-        if (window.pageYOffset >= sticky) {
-            // if so, ad the fixed class
-            sectionNav.classList.add('sticky');
-        } else {
-            sectionNav.className.replace(' sticky', '');
-        }
-    });
+//     window.addEventListener('scroll', (event) => {
+//         let sectionNav = document.getElementById('section-nav-menu');
+//         let sticky = sectionNav.offsetTop;
+//         // whether that's below the form
+//         if (window.pageYOffset >= sticky) {
+//             // if so, ad the fixed class
+//             sectionNav.classList.add('sticky');
+//         } else {
+//             sectionNav.className.replace(' sticky', '');
+//         }
+//     });
 
-});
+// });
