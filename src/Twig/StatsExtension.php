f361cf1d1c648b83fc839b697704df36cd3f22a1
Kevin Tourret <tourret.kevin@gmail.com>
2021-06-28 20:27:26 +0200

    add css color for stats

--- src/Twig/StatsExtension.php
+++ src/Twig/StatsExtension.php
@@ -24,10 +24,31 @@ class StatsExtension extends AbstractExtension
      * @param int $stats
      * @return float
      */
-    public function getPercentByStats(int $stats) {
+    public function getPercentByStats(int $stats): float {
         $percent = ($stats/200) * 100;
         return $percent > 100 ? 100 : $percent;
     }
 
+    /**
+     * @param int $stats
+     * @return string
+     */
+    public function getCssForStats(int $stats): string {
+        if ($stats <= 40) {
+            return 'stats-very-low';
+        } else if ($stats > 40 && $stats <= 60) {
+            return 'stats-low';
+        } else if ($stats > 60 && $stats <= 80) {
+            return 'stats-average-low';
+        } else if ($stats > 80 && $stats <= 100) {
+            return 'stats-average';
+        } else if ($stats > 100 && $stats <= 130) {
+            return 'stats-high-low';
+        } else if ($stats > 130 && $stats <= 160) {
+            return 'stats-high';
+        } else {
+            return 'stats-very-high';
+        }
+    }
 
 }
