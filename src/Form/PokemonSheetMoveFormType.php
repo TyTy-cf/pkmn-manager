354d4a337e8172348077051656d7078f674d95ce
Kevin Tourret <tourret.kevin@gmail.com>
2021-09-07 16:37:07 +0200

    Dynamic form for moves working ; WIP : adding them to the pokemon_sheet

--- src/Form/PokemonSheetMoveFormType.php
+++ src/Form/PokemonSheetMoveFormType.php
@@ -44,32 +44,32 @@ class PokemonSheetMoveFormType extends AbstractType
             ->add('moves', CollectionType::class, [
                 'label' => false,
                 'by_reference' => false,
-                'entry_type' => MoveFormType::class,
                 'allow_add' => true,
                 'allow_delete' => true,
                 'add_btn_icon' => 'fas fa-plus-circle',
-                'add_btn_class' => 'btn pokemon-sheet-add-button',
+                'add_btn_class' => 'btn pokemon-sheet-add-button mr-1',
                 'add_btn_label' => false,
                 'add_btn_label_translation_domain' => false,
-                'delete_btn_class' => 'btn pokemon-sheet-cancel-button',
+                'delete_btn_class' => 'btn pokemon-sheet-cancel-button mx-1 mb-4',
                 'delete_btn_label' => false,
                 'delete_btn_icon' => 'fas fa-trash-alt',
                 'delete_btn_label_translation_domain' => false,
                 'attr' => [
                     'class' => 'mt-2 mb-2',
                 ],
+                'entry_type' => MoveFormType::class,
                 'entry_options' => [
                     'data' => $moves,
                     'collapsable' => true,
                     'attr' => [
                         'data-form-collapsable' => false,
-                        'class' => 'mt-2',
+                        'class' => 'col-6',
                     ],
                     'label' => false,
                 ],
             ])
             ->add('submit', SubmitType::class, [
-                'label' => 'pokemon_sheet.form.submit',
+                'label' => 'pokemon_sheet.form.add',
                 'attr' => [
                     'class' => 'btn btn-primary ml-1 btn-search-pokemon',
                 ],
